7ad5cb5ca540f4fe44752c0d75150dd07cdf6634
bouska.j11@gmail.com <bouska.j11@gmail.com@57a4f66d-e069-b5d7-4cda-729e5cf94b3c>
2014-05-27 08:20:16 +0000

    zapomeute slozky

--- CarRental/src/query/XQuery.java
+++ CarRental/src/query/XQuery.java
@@ -3,10 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package query;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -19,55 +17,51 @@ import net.sf.saxon.query.XQueryExpression;
 import net.sf.saxon.trans.XPathException;
 
 public class XQuery {
-    
-    public final static String out = System.getProperty("user.dir")+"/output/";
 
-    public void  callQuery() throws XPathException, IOException {
-        
+    public final static String out = System.getProperty("user.dir") + "/output/";
+
+    public void callQuery() throws XPathException, IOException {
+String out =  new java.io.File( "." ).getCanonicalPath() + "/output/";
         Configuration config = new Configuration();
 
         StaticQueryContext staticContext = config.newStaticQueryContext();
 
-        XQueryExpression customer = staticContext.compileQuery(fileToInputStream("xQuery/queryCustomer.xq"),"UTF-8" );
-        XQueryExpression customerDet = staticContext.compileQuery(fileToInputStream("xQuery/queryCustomerMoreDetails.xq"),"UTF-8");
-        XQueryExpression car = staticContext.compileQuery(fileToInputStream("xQuery/queryCar.xq"),"UTF-8");
-        XQueryExpression carDet = staticContext.compileQuery(fileToInputStream("xQuery/queryCarMoreDetails.xq"),"UTF-8");
-        XQueryExpression average = staticContext.compileQuery(fileToInputStream("xQuery/queryAverage.xq"),"UTF-8");
-        XQueryExpression lease = staticContext.compileQuery(fileToInputStream("xQuery/queryLease.xq"),"UTF-8");
-        
-        DynamicQueryContext dynamicContext =
-                new DynamicQueryContext(config);
+        XQueryExpression customer = staticContext.compileQuery(fileToInputStream("xQuery/queryCustomer.xq"), "UTF-8");
+        XQueryExpression customerDet = staticContext.compileQuery(fileToInputStream("xQuery/queryCustomerMoreDetails.xq"), "UTF-8");
+        XQueryExpression car = staticContext.compileQuery(fileToInputStream("xQuery/queryCar.xq"), "UTF-8");
+        XQueryExpression carDet = staticContext.compileQuery(fileToInputStream("xQuery/queryCarMoreDetails.xq"), "UTF-8");
+        XQueryExpression average = staticContext.compileQuery(fileToInputStream("xQuery/queryAverage.xq"), "UTF-8");
+        XQueryExpression lease = staticContext.compileQuery(fileToInputStream("xQuery/queryLease.xq"), "UTF-8");
+
+        DynamicQueryContext dynamicContext
+                = new DynamicQueryContext(config);
 
         Properties props = new Properties();
         props.setProperty(OutputKeys.METHOD, "xml");
         props.setProperty(OutputKeys.INDENT, "yes");
 
-        customer.run(dynamicContext, new StreamResult(out+"outputCustomer.xml"), props);
+        customer.run(dynamicContext, new StreamResult(out + "outputCustomer.xml"), props);
         customer.run(dynamicContext, new StreamResult(System.out), props);
-        
-        customerDet.run(dynamicContext, new StreamResult(out+"outputCustomerMoreDetails.xml"), props);
+
+        customerDet.run(dynamicContext, new StreamResult(out + "outputCustomerMoreDetails.xml"), props);
         customerDet.run(dynamicContext, new StreamResult(System.out), props);
-        
-        car.run(dynamicContext, new StreamResult(out+"outputCar.xml"), props);
+
+        car.run(dynamicContext, new StreamResult(out + "outputCar.xml"), props);
         car.run(dynamicContext, new StreamResult(System.out), props);
-        
-        carDet.run(dynamicContext, new StreamResult(out+"outputCarMoreDetails.xml"), props);
+
+        carDet.run(dynamicContext, new StreamResult(out + "outputCarMoreDetails.xml"), props);
         carDet.run(dynamicContext, new StreamResult(System.out), props);
-        
-        average.run(dynamicContext, new StreamResult(out+"outputAverage.xml"), props);
+
+        average.run(dynamicContext, new StreamResult(out + "outputAverage.xml"), props);
         average.run(dynamicContext, new StreamResult(System.out), props);
-        
-        lease.run(dynamicContext, new StreamResult(out+"outputLease.xml"), props);
+
+        lease.run(dynamicContext, new StreamResult(out + "outputLease.xml"), props);
         lease.run(dynamicContext, new StreamResult(System.out), props);
-        
+
     }
-    
-    
-    private InputStream fileToInputStream(String file)
-    {
-        
-        
-        
+
+    private InputStream fileToInputStream(String file) {
+
         return this.getClass().getClassLoader().getResourceAsStream(file);
     }
 }
